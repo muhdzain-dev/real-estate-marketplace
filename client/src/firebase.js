eb09ffb09535469aff08499be319ee7f8ce779ae
Muhammad Zain <muhd.zain.dev@gmail.com>
2024-10-06 00:08:50 +0400

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAo0DQJUfIQ847GutGEeogYrosO8ZViN7g",
-  authDomain: "mern-estate-413d4.firebaseapp.com",
-  projectId: "mern-estate-413d4",
-  storageBucket: "mern-estate-413d4.appspot.com",
-  messagingSenderId: "639547171625",
-  appId: "1:639547171625:web:857c7ddcd768ad01128d38",
+ 
 };
 
 // Initialize Firebase
